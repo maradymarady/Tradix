ca3888d217c2495f7df244312886bd78e79043d3
X RDI <maradymarady06@gmail.com>
2026-07-02 16:58:51 +0700

    fix supabase cookie handler

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -9,14 +9,16 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-        set(name: string, value: string, options: any) {
-          cookieStore.set({ name, value, ...options });
-        },
-        remove(name: string, options) {
-          cookieStore.set({ name, value: '', ...options: any });
+  get(name: string) {
+    return cookieStore.get(name)?.value;
+  },
+
+  set(name: string, value: string, options: any) {
+    cookieStore.set({ name, value, ...options });
+  },
+
+  remove(name: string, options: any) {
+    cookieStore.set({ name, value: '', ...options });
         },
       },
     }
